018564d7d25cd11dbb9f2e65673ce573b7791c7b
Raymond <rayjxclusive@gmail.com>
2021-08-17 19:44:21 +0300

    update created container for the ToDo list

---
+++ src/containers/ToDoListContainter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {connect} from 'react-redux';
+import ToDoList from '../components/ToDoList';
+
+
+const mapStateToProps = (state) => ({
+ toDoList: state.toDoList,
+});
+
+const ToDoListContainer = connect(mapStateToProps)(ToDoList);
+
+export default ToDoListContainer;
