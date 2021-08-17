1fb7d971536b8982e85025adb34f43764de31b57
Raymond <rayjxclusive@gmail.com>
2021-08-17 17:48:19 +0300

    created sample skeletons of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Container>
+        <Row className="row">
+             <Col xl={12}>
+                  <h1>My funny react play ground</h1>
+                  <Button>Pesh</Button>
+             </Col>
+        </Row>
+     </Container>
   );
 }
 
