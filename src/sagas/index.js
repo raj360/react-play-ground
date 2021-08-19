b44655903c2e4d9047842b3cfe1088cfb69988ee
Raymond <rayjxclusive@gmail.com>
2021-08-19 10:40:40 +0300

    update added generator function to loadToDoList from end point

---
+++ src/sagas/index.js
@@ -0,0 +1,20 @@
+import {put,call,takeEvery,all} from 'redux-saga/effects'
+import {LOAD_TODO_LIST,RENDER_TODO_LIST} from '../actions'
+
+
+export function *fetchToDoList(){
+
+}
+
+
+export function *loadToDoList(){
+  const endpoint = 'https://gist.githubusercontent.com/brunokrebs/f1cacbacd53be83940e1e85860b6c65b/raw/to-do-items.json';
+  const response = yield call(fetch,endpoint);
+  const data = yield response.json();
+  yield put({type:RENDER_TODO_LIST,toDoList:data});
+}
+
+
+export function *rootSaga() {
+   
+}
