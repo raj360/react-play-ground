f80842529606b5b7404296df28a0713b5768adb1
Raymond <rayjxclusive@gmail.com>
2021-08-19 13:38:25 +0300

    securing the app using auth0

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-
 export default () => (
     <div className = "p-2">
         <Link  to="/" className = "btn btn-primary" style={{marginRight: 10}} > To-Do List</Link>
