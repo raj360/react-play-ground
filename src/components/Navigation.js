e1195446382ae87fe61c74f6e36277dd34474779
Raymond <rayjxclusive@gmail.com>
2021-08-19 13:12:49 +0300

    update improve spacing between navigation links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,7 @@ import {Link} from 'react-router-dom';
 
 export default () => (
     <div className = "p-2">
-        <Link  to="/" className = "btn btn-primary" > To-Do List</Link>
-      
+        <Link  to="/" className = "btn btn-primary" style={{marginRight: 10}} > To-Do List</Link>
         <Link  to="/new-item"  className="btn btn-secondary"> + Add New</Link>
     </div>
 )
